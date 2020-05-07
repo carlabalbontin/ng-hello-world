130fa9d9d9ed87984f895e1254a517cd70da49b5
Carla Balbontín Tanhnuz <cbalbontin@GST-CBALBON-MAC.local>
2020-05-07 12:43:41 -0400

    Two-way binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,11 +4,13 @@ import { CoursesService } from './courses.service';
 @Component({
   selector: 'courses',
   template: `
-    <input #email (keyup.enter)="onKeyEnter(email.value)"/>
+    <input [(ngModel)]="email" (keyup.enter)="onKeyEnter()"/>
   `
 })
 export class CoursesComponent {
-  onKeyEnter(email) {
-    console.log(email);
+  email = 'me@example.com';
+
+  onKeyEnter() {
+    console.log(this.email);
   }
 }
