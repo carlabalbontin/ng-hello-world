c0a0170c43c60f7b36a95635c979b558b3af0a53
Carla Balbontín Tanhnuz <cbalbontin@GST-CBALBON-MAC.local>
2020-05-07 10:48:18 -0400

    Property binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,6 +5,7 @@ import { CoursesService } from './courses.service';
   selector: 'courses',
   template: `
     <h2>{{ title }}</h2>
+    <img src="{{ imageUrl }}" />
     <ul>
       <li *ngFor="let course of courses">{{ course }}</li>
     </ul>
@@ -12,6 +13,7 @@ import { CoursesService } from './courses.service';
 })
 export class CoursesComponent {
   title = 'List of courses';
+  imageUrl = 'http://lorempixel.com/400/200';
   courses;
 
   constructor(service: CoursesService) {
