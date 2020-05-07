2f301337252430f31384311b51cadb3ac7ab1ef2
Carla Balbontín Tanhnuz <cbalbontin@GST-CBALBON-MAC.local>
2020-05-07 11:46:10 -0400

    Style and Class binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,19 +4,9 @@ import { CoursesService } from './courses.service';
 @Component({
   selector: 'courses',
   template: `
-    <h2>{{ title }}</h2>
-    <img src="{{ imageUrl }}" />
-    <ul>
-      <li *ngFor="let course of courses">{{ course }}</li>
-    </ul>
+    <button class="btn btn-primary" [class.pink]="isActive">Save</button>
   `
 })
 export class CoursesComponent {
-  title = 'List of courses';
-  imageUrl = 'http://lorempixel.com/400/200';
-  courses;
-
-  constructor(service: CoursesService) {
-    this.courses = service.getCourses();
-  }
+  isActive = true;
 }
