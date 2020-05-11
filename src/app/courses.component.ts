b0d7345db22ebc9f0b4b99e6606c7b87ba4d708a
Carla Balbontín Tanhnuz <cbalbontin@GST-CBALBON-MAC.local>
2020-05-11 11:44:24 -0400

    ngFor trackBy

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,13 +4,39 @@ import { CoursesService } from './courses.service';
 @Component({
   selector: 'courses',
   template: `
-    <div>
-      <input [(ngModel)]="userInput" />
-    </div>
-    <p>{{ userInput | titleCase }}
+    <section>
+    <h2>NgFor</h2>
+    <button (click)="loadCourses()">Load Courses</button>
+    <ul>
+      <li *ngFor="let course of courses; index as i; trackBy: trackCourses">
+        {{ i }} - {{ course.name }}
+      </li>
+    </ul>
+  </section>
 
   `
 })
 export class CoursesComponent {
-  userInput = '';
+  courses;
+
+  loadCourses() {
+    this.courses = [
+      {
+        id: 1,
+        name: 'First course'
+      },
+      {
+        id: 2,
+        name: 'Second course'
+      },
+      {
+        id: 3,
+        name: 'Third course'
+      }
+    ];
+  }
+
+  trackCourses(index, course) {
+    return course ? course.id : undefined;
+  }
 }
