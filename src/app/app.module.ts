46494e7dea4df6a122df664ea2b4dc124ed9e21e
Carla Balbontín Tanhnuz <cbalbontin@GST-CBALBON-MAC.local>
2020-05-12 11:36:57 -0400

    Zippy Component Toggle Assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FavoriteComponent } from './favorite/favorite.component';
 import { BootstrapListGroupComponent } from './bootstrap-list-group/bootstrap-list-group.component';
 import { LikeComponent } from './like/like.component';
 import { InputFormatDirective } from './input-format.directive';
+import { ZippyComponent } from './zippy/zippy.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { InputFormatDirective } from './input-format.directive';
     FavoriteComponent,
     BootstrapListGroupComponent,
     LikeComponent,
-    InputFormatDirective
+    InputFormatDirective,
+    ZippyComponent
   ],
   imports: [
     BrowserModule,
