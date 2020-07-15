79971631846ca34ae97dc8d8a3abb5816c48d0af
Carla Balbontín Tanhnuz <cbalbontin@cap-vcisternan.inacap.cl>
2020-07-15 10:37:55 -0400

    Http: Optimistic vs Pessimistic Updates

--- src/app/http/posts/posts.component.ts
+++ src/app/http/posts/posts.component.ts
@@ -17,23 +17,20 @@ export class PostsComponent implements OnInit {
 
   ngOnInit() {
     this.service.getAll()
-      .subscribe(
-        response => {
-          this.posts = response;
-        }
-      );
+      .subscribe(response => this.posts = response);
   }
 
   createPost(input: HTMLInputElement) {
     const post = { title: input.value };
+    this.posts.splice(0, 0, post);    // We "optimiscally" add the post before the call to the server
     input.value = '';
     this.service.create(post)
       .subscribe(
         response => {
           post['id'] = response['id'];
-          this.posts.splice(0, 0, post);
         },
         (error: AppError) => {
+          this.posts.splice(0, 1);    // If there's an error, we get rid of the post we "optimistically" added
           if (error instanceof BadInputError) {
             // this.form.setErrors(error.originalError)
           } else {
@@ -45,22 +42,18 @@ export class PostsComponent implements OnInit {
 
   updatePost(post) {
     this.service.update(post)
-      .subscribe(
-        response => {
-          console.log(response);
-        }
-      );
+      .subscribe(response => console.log(response));
   }
 
   deletePost(post) {
-    // this.service.deletePost('hola/' + post.id) // to test error
+    const index = this.posts.indexOf(post); // optimistic update
+    this.posts.splice(index, 1);            // optimistic update
+
     this.service.delete(post.id)
       .subscribe(
-        response => {
-          const index = this.posts.indexOf(post);
-          this.posts.splice(index, 1);
-        },
+        () => {},
         (error: AppError) => {
+          this.posts.splice(index, 0, post);  // rollback optimistic update if there is an error
           if (error instanceof NotFoundError) {
             console.log('This post has already been deleted');
           } else {
